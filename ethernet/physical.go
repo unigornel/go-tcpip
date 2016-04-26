0cd1f6c95d83d16f15eaf8434ac048e3dbbc5d2d
Henri Verroken <henriverroken@gmail.com>
2016-04-26 13:20:35 +0200

    Make NIC a closer, not a start/stopper

--- ethernet/physical.go
+++ ethernet/physical.go
@@ -8,4 +8,5 @@ type NIC interface {
 	Send() chan<- Packet
 	Receive() <-chan Packet
 	GetMAC() MAC
+	Close()
 }
--- ethernet/physical_unigornel.go
+++ ethernet/physical_unigornel.go
@@ -1,14 +1,41 @@
 package ethernet
 
-type miniosNIC struct{}
+// #cgo LDFLAGS: -Wl,--unresolved-symbols=ignore-in-object-files
+// #include <mini-os/network.h>
+// #include <mini-os/types.h>
+//
+// extern void *memcpy(void *, const void *, size_t);
+// extern void *malloc(size_t);
+// extern void free(void *);
+import "C"
+
+type miniosNIC struct {
+	tx chan Packet
+	rx chan Packet
+}
 
 // NewNIC creates a new NIC linked to the Mini-OS network.
 func NewNIC() NIC {
-	return new(miniosNIC)
+	nic := new(miniosNIC)
+
+	nic.tx = make(chan Packet)
+	nic.rx = make(chan Packet)
+	go nic.sendAll()
+
+	return nic
+}
+
+func (nic *miniosNIC) Close() {
+	if nic.tx == nil || nic.rx == nil {
+		panic("NIC not started")
+	}
+
+	close(nic.tx)
+	close(nic.rx)
 }
 
 func (nic *miniosNIC) Send() chan<- Packet {
-	panic("not implemented")
+	return nic.tx
 }
 
 func (nic *miniosNIC) Receive() <-chan Packet {
@@ -17,4 +44,7 @@ func (nic *miniosNIC) Receive() <-chan Packet {
 
 func (nic *miniosNIC) GetMAC() MAC {
 	panic("not implemented")
+}
+
+func (nic *miniosNIC) sendAll() {
 }
